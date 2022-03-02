81eb38bee1302e72752c078c02c70750f60520c3
jmorettoni <jmorettoni@gmail.com>
2022-03-02 12:54:30 -0300

    up network infos

--- src/components/Install.jsx
+++ src/components/Install.jsx
@@ -1,8 +1,8 @@
 const Install = () => {
     return (
       <div>
-        <h3 style={{width:"100%", textAlign:"center"}}><br/><br/><br/>Please, connect with Google Chrome and connect with the metamask.</h3> 
-        <p  style={{width:"100%", textAlign:"center"}} >If you dont already install the metamask in the google Chrome,  Follow the link to install 👇🏼 </p>
+        <h3 style={{width:"100%", textAlign:"center"}}><br/><br/><br/>Please, connect with the metamask.</h3> 
+        <p  style={{width:"100%", textAlign:"center"}} >If you dont already install the metamask on your browser, follow the link to install. 👇🏼 </p>
         <p  style={{width:"100%", textAlign:"center"}} ><a href="https://metamask.io/download.html" >Meta Mask</a></p>
       </div>
     );
